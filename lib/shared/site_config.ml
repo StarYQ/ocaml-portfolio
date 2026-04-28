b1ac74035cfaa356f9443e922fd74bf7bfc7a9ba
Arnab Bhowmik <46697160+StarYQ@users.noreply.github.com>
2026-04-28 14:23:36 -0400

    config: hide 'me' page

--- lib/shared/site_config.ml
+++ lib/shared/site_config.ml
@@ -2,4 +2,4 @@ open! Core
 
 (* Toggle this to [false] to remove the personal Me page from the public site.
    When disabled, the ME nav item is hidden and /me falls back to Home. *)
-let show_me_page = true
+let show_me_page = false
